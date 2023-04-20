e3f203f5531a06c0ef41365a6c1b0aa0186ec165
Richard <richardkwon5@gmail.com>
2023-04-19 19:51:36 -0700

    glb file loaded

--- components/utils/threejs.ts
+++ components/utils/threejs.ts
@@ -1,50 +1,45 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import Globe from '../assets/earth.jpg';
-import MilkyWay from '../assets/milky_way.jpg';
+import { GLTF, GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import Globe from '../../assets/earth.jpg';
+import MilkyWay from '../../assets/milky_way.jpg';
+import RealGlobe from '../../assets/Earth_1_12756.glb'
 
-interface UniformsType {
-  [uniform: string]: {value: any; type: string}
-}
 
-const vertexShader = `
-  varying vec3 vNormal;
-  varying vec3 vWorldPosition;
+const loadGlobeGLB = (scene: THREE.Scene): void => {
+  const loader: GLTFLoader = new GLTFLoader();
+  loader.load(
+    RealGlobe,
+    (gltf: GLTF) => {
+      console.log('GLB file loaded successfully:', gltf);
+      scene.add(gltf.scene);
+      const box = new THREE.Box3().setFromObject(gltf.scene);
+      const center = box.getCenter(new THREE.Vector3());
+      gltf.scene.position.sub(center); // center the model
+      gltf.scene.scale.multiplyScalar(10 / box.getSize(new THREE.Vector3()).length()); // normalize the model size
 
-  void main() {
-    vNormal = normalize(normalMatrix * normal);
-    vec4 worldPosition = modelMatrix * vec4(position, 1.0);
-    vWorldPosition = worldPosition.xyz;
+    },
+    undefined,
+    (error: ErrorEvent) => {
+      console.error('An error occurred while loading the GLB file:', error);
+    }
+  );
+};
 
-    gl_Position = projectionMatrix * viewMatrix * worldPosition;
-  }
-`;
-
-const fragmentShader = `
-  uniform vec3 glowColor;
-  uniform float alpha;
-  varying vec3 vNormal;
-  varying vec3 vWorldPosition;
-
-  void main() {
-    vec3 viewDirection = normalize(cameraPosition - vWorldPosition);
-    float intensity = pow(1.0 - dot(vNormal, viewDirection), 0.1);
-    gl_FragColor = vec4(glowColor, alpha) * intensity;
-  }
-`;
 
 const createGlobe = (canvas: HTMLCanvasElement): void => {
   // Set up the scene, camera, and renderer
   const scene: THREE.Scene = new THREE.Scene();
-  const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(100, canvas.width / canvas.height, 1, 1000);
+  const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(75, canvas.width / canvas.height, 1, 1000);
   const renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer({ canvas , antialias: true });
   renderer.setSize(canvas.clientWidth, canvas.clientHeight);
   renderer.setPixelRatio(window.devicePixelRatio)
 
-  // Set up the globe geometry and material
-  const geometry: THREE.SphereGeometry = new THREE.SphereGeometry(5, 100, 100);
-  const texture: THREE.Texture = new THREE.TextureLoader().load(Globe.src);
-  const material: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({ map: texture });
+
+  // Add ambient light
+  const ambientLight = new THREE.AmbientLight(0xffffff, 2);
+  scene.add(ambientLight);
+
 
   // Set up the Milky Way texture and its position
   const milkyWayTexture: THREE.Texture = new THREE.TextureLoader().load(MilkyWay.src);
@@ -53,28 +48,11 @@ const createGlobe = (canvas: HTMLCanvasElement): void => {
   milkyWayTexture.repeat.set(1, 1);
   scene.background = milkyWayTexture;
 
-  // Only for debugging, set background color to white
+  //White background for debugging
   // scene.background = new THREE.Color(0xffffff);
 
-  // Create a mesh with the geometry and material and add it to the scene
-  const globe: THREE.Mesh = new THREE.Mesh(geometry, material);
-  scene.add(globe);
-
-  // Create a mesh for the glow effect
-  const glowGeometry: THREE.SphereGeometry = new THREE.SphereGeometry(5.1, 50, 50);
-  const glowMaterial: THREE.ShaderMaterial = new THREE.ShaderMaterial({
-    uniforms: {
-      glowColor: { value: new THREE.Color(0x0000A5), type: 'c' },
-      alpha: { value: 0.3, type: 'f' },
-    } as UniformsType,
-    vertexShader: vertexShader,
-    fragmentShader: fragmentShader,
-    side: THREE.BackSide, // Change this line
-    blending: THREE.AdditiveBlending,
-    transparent: true,
-  });
-  const glowMesh: THREE.Mesh = new THREE.Mesh(glowGeometry, glowMaterial);
-  globe.add(glowMesh);
+  // Load and display the GLB file
+  loadGlobeGLB(scene);
 
   // Set up orbit controls
   const controls = new OrbitControls(camera, renderer.domElement);
--- next.config.js
+++ next.config.js
@@ -3,4 +3,22 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  webpack: (config, { isServer }) => {
+    // Add file-loader for .glb files
+    config.module.rules.push({
+      test: /\.glb$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          publicPath: '/_next/static/files',
+          outputPath: `${isServer ? '../' : ''}static/files`,
+          name: '[name].[contenthash].[ext]',
+          esModule: false,
+        },
+      },
+    });
+
+    return config;
+  },
+};
